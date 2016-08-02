fa41b046505312b233dc2f8df36b39bb396d5da2
Nicole <nhswong@gmail.com>
2016-08-02 13:17:13 -0700

    Added Q9

---
+++ 9_book_chapters.rb
@@ -0,0 +1,43 @@
+# 9. Build a Ruby class called "Book". Objects of the Book class must have two attributes: title and chapters. Objects must have two methods: add_chapter and chapters. The `add_chapter` methods adds a chapter by giving it a title. The chapters method should display the number of chapters and lists all the chapters as in:
+
+# book = Book.new
+# book.title = "My Awesome Book"
+# book.add_chapter("My Awesome Chapter 1")
+# book.add_chapter("My Awesome Chapter 2")
+# book.chapters
+
+# # This should print:
+# # Your book: My Awesome Book has 2 chapters:
+# # 1. My Awesome Chapter 1
+# # 2. My Awesome Chapter 2
+
+
+class Book
+  attr_accessor :title, :chapters
+  def initialize
+    @title = title
+    @chapters = []
+  end
+
+  def add_chapter(chapter)
+    @chapters << chapter
+  end
+
+  def chapters
+    puts "Your Book: #{@title} has #{@chapters.length} chapters"
+    counter = 0
+    @chapters.each do |chpt|
+      counter += 1
+      puts "#{counter}. #{chpt}"
+    end
+
+
+  end
+end
+
+
+book = Book.new
+book.title = "My Awesome Book"
+book.add_chapter("My Awesome Chapter 1")
+book.add_chapter("My Awesome Chapter 2")
+book.chapters
